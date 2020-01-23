b393bd415d517840c967ae6bb1345c3eb067eb52
yukimura1227 <takamura1227@gmail.com>
2020-01-23 21:17:15 +0900

    feat: chalkを導入して、色付きで表示させるように改善

--- bin/jobcan.js
+++ bin/jobcan.js
@@ -2,8 +2,9 @@
 'use strict';
 
 const figlet = require("figlet");
+const chalk = require("chalk");
 const args = process.argv.slice(2);
 
 figlet(args, function(err, data) {
-  console.log(data);
+  console.log(chalk.blue.bgRed.bold(data));
 });
